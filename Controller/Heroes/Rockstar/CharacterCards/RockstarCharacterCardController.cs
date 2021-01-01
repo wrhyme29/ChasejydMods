ea8dacb65684359b42b01faa9b46f3c92526c7d5
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-01 15:08:01 -0600

    Added Controllers

---
+++ Controller/Heroes/Rockstar/CharacterCards/RockstarCharacterCardController.cs
@@ -0,0 +1,41 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chasejyd.Rockstar
+{
+	public class RockstarCharacterCardController : HeroCharacterCardController
+	{
+		public RockstarCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+		{
+		}
+		public override IEnumerator UsePower(int index = 0)
+		{
+			
+			yield break;
+		}
+		public override IEnumerator UseIncapacitatedAbility(int index)
+		{
+			switch (index)
+			{
+				case 0:
+					{
+						yield break;
+					}
+				case 1:
+					{
+						
+						yield break;
+					}
+				case 2:
+					{
+						
+						yield break;
+					}
+			}
+			yield break;
+		}
+	}
+}
