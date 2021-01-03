f8ffa1ab91568acd26ae4debc0fec832958edb68
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-02 19:35:07 -0600

    Added BlondeAmbition Controller

--- Controller/Heroes/Rockstar/Cards/BlondeAmbitionCardController.cs
+++ Controller/Heroes/Rockstar/Cards/BlondeAmbitionCardController.cs
@@ -3,6 +3,7 @@ using Handelabra.Sentinels.Engine.Model;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Chasejyd.Rockstar
 {
@@ -14,6 +15,47 @@ namespace Chasejyd.Rockstar
 
         }
 
+        public override IEnumerator Play()
+        {
+            //Deal one Target 2 Melee damage. Deal a second target 2 Projectile Damage. 
 
+            List<DealDamageAction> targets = new List<DealDamageAction>();
+            IEnumerator firstDamage = base.GameController.SelectTargetsAndDealDamage(this.DecisionMaker, new DamageSource(base.GameController, base.CharacterCard), 2, DamageType.Melee, new int?(1), false, new int?(1), storedResultsDamage: targets, addStatusEffect: RemoveEndOfTurnResponse, cardSource: GetCardSource());
+            IEnumerator secondDamage = base.GameController.SelectTargetsAndDealDamage(this.DecisionMaker, new DamageSource(base.GameController, base.CharacterCard), 2, DamageType.Projectile, new int?(1), false, new int?(1), additionalCriteria: (Card c) => !(from d in targets
+                                                                                                                                                                                                                                                                  select d.Target).Contains(c), addStatusEffect: RemoveEndOfTurnResponse, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(firstDamage);
+                yield return base.GameController.StartCoroutine(secondDamage);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(firstDamage);
+                base.GameController.ExhaustCoroutine(secondDamage);
+            }
+           
+            yield break;
+        }
+
+        private IEnumerator RemoveEndOfTurnResponse(DealDamageAction dd)
+        {
+            //Non-character-card targets dealt damage this way may not use any End of Turn abilities until the start of {Rockstar}'s next turn.
+
+            if (dd != null && dd.DidDealDamage && dd.Target != null && !dd.Target.IsCharacter)
+            {
+                PreventPhaseEffectStatusEffect preventPhaseEffectStatusEffect = new PreventPhaseEffectStatusEffect();
+                preventPhaseEffectStatusEffect.UntilStartOfNextTurn(TurnTaker);
+                preventPhaseEffectStatusEffect.CardCriteria.IsSpecificCard = dd.Target;
+                IEnumerator coroutine = AddStatusEffect(preventPhaseEffectStatusEffect);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+        }
     }
 }
