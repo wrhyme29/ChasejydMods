4020aef49962b113abb077016fc450338a8880d0
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-02 20:11:36 -0600

    Added HitMeBaby Controller

--- Controller/Heroes/Rockstar/Cards/HitMeBabyCardController.cs
+++ Controller/Heroes/Rockstar/Cards/HitMeBabyCardController.cs
@@ -11,9 +11,48 @@ namespace Chasejyd.Rockstar
 
         public HitMeBabyCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
-
+            SpecialStringMaker.ShowHasBeenUsedThisTurn(FirstTimePlayOrPower).Condition = () => Game.ActiveTurnTaker != TurnTaker;
         }
 
 
+        private const string FirstTimePlayOrPower = "FirstTimePlayOrPower";
+
+        public override IEnumerator Play()
+        {
+            //When this card enters play, {Rockstar} gains 2 HP.
+            IEnumerator coroutine = GameController.GainHP(CharacterCard, 2, cardSource: GetCardSource());
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+
+        public override void AddTriggers()
+        {
+            //The first time per turn outside of her own that another Hero allows {Rockstar} to Use a Power or Play a Card, she may first Play a Card.
+            AddTrigger<PlayCardAction>((PlayCardAction pca) => Game.ActiveTurnTaker != TurnTaker && pca.ResponsibleTurnTaker == TurnTaker && !HasBeenSetToTrueThisTurn(FirstTimePlayOrPower), PlayCardResponse, TriggerType.PlayCard, TriggerTiming.Before);
+            AddTrigger<UsePowerAction>((UsePowerAction upa) => Game.ActiveTurnTaker != TurnTaker && upa.HeroUsingPower == HeroTurnTakerController && !HasBeenSetToTrueThisTurn(FirstTimePlayOrPower), PlayCardResponse, TriggerType.PlayCard, TriggerTiming.Before);
+            AddAfterLeavesPlayAction((GameAction ga) => ResetFlagAfterLeavesPlay(FirstTimePlayOrPower), TriggerType.Hidden);
+        }
+
+        private IEnumerator PlayCardResponse(GameAction action)
+        {
+            SetCardPropertyToTrueIfRealAction(FirstTimePlayOrPower);
+            IEnumerator coroutine = SelectAndPlayCardFromHand(HeroTurnTakerController);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
     }
 }
