41e6ae2af84dcb47181642fa61c170a07faab5bd
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-02 20:45:13 -0600

    Added PlayToTheCrowd Controller

--- Controller/Heroes/Rockstar/Cards/PlayToTheCrowdCardController.cs
+++ Controller/Heroes/Rockstar/Cards/PlayToTheCrowdCardController.cs
@@ -3,6 +3,7 @@ using Handelabra.Sentinels.Engine.Model;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Chasejyd.Rockstar
 {
@@ -14,6 +15,44 @@ namespace Chasejyd.Rockstar
 
         }
 
+        public override void AddTriggers()
+        {
+            //Reduce Melee and Projectile Damage dealt to {Rockstar} by 1.
+            AddReduceDamageTrigger((DealDamageAction dd) => dd.Target == CharacterCard && (dd.DamageType == DamageType.Melee || dd.DamageType == DamageType.Projectile), (DealDamageAction dd) => 1);
 
+            //Whenever a Non-Hero card would deal damage to the Hero Target with the lowest HP, or any Target with 3 or fewer HP, you may redirect that damage to {Rockstar}.
+            Func<DealDamageAction, bool> criteria = (DealDamageAction dealDamage) => (!dealDamage.DamageSource.Card.IsHero && GameController.IsCardVisibleToCardSource(dealDamage.DamageSource.Card, GetCardSource())) && (dealDamage.Target.IsHero && GameController.IsCardVisibleToCardSource(dealDamage.Target, GetCardSource())) && (CanCardBeConsideredLowestHitPoints(dealDamage.Target, (Card c) => c.IsHero && GameController.IsCardVisibleToCardSource(c, GetCardSource())) || dealDamage.Target.HitPoints <= 3);
+            AddTrigger(criteria, RedirectDamageResponse, TriggerType.RedirectDamage, TriggerTiming.Before);
+        }
+
+        private IEnumerator RedirectDamageResponse(DealDamageAction dd)
+        {
+            List<bool> shouldRedirect = new List<bool>();
+            IEnumerator coroutine = DetermineIfGivenCardIsTargetWithLowestOrHighestHitPoints(dd.Target, highest: false, (Card card) => card.IsHero && GameController.IsCardVisibleToCardSource(card, GetCardSource()), dd, shouldRedirect);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            if (dd.Target.HitPoints > 3 && !shouldRedirect.First())
+            {
+                yield break;
+            }
+
+            coroutine = GameController.RedirectDamage(dd, CharacterCard, isOptional: true, cardSource: GetCardSource());
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+
+            yield break;
+        }
     }
 }
