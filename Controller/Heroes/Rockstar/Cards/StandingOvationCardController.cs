dff51df6800372e0180727509022c37b2014051c
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-02 21:27:50 -0600

    Added StandingOvation Controller

--- Controller/Heroes/Rockstar/Cards/StandingOvationCardController.cs
+++ Controller/Heroes/Rockstar/Cards/StandingOvationCardController.cs
@@ -14,6 +14,30 @@ namespace Chasejyd.Rockstar
 
         }
 
+		public override void AddTriggers()
+		{
+			//When a Non-Hero Target is destroyed outside of {Rockstar}'s turn, {Rockstar} may draw a card."
 
-    }
+			AddTrigger((DestroyCardAction destroyCard) => !destroyCard.CardToDestroy.Card.IsHero && destroyCard.CardToDestroy.Card.IsTarget && destroyCard.WasCardDestroyed && Game.ActiveTurnTaker != TurnTaker, DrawCardResponse, TriggerType.DrawCard, TriggerTiming.After);
+		}
+
+		private IEnumerator DrawCardResponse(DestroyCardAction destroyCard)
+		{
+			string message = $"{Card.Title} allows {TurnTaker.Name} to draw a card.";
+			IEnumerator coroutine = GameController.SendMessageAction(message, Priority.Medium, GetCardSource());
+			IEnumerator drawCardE = DrawCard(null, optional: true);
+			if (base.UseUnityCoroutines)
+			{
+				yield return base.GameController.StartCoroutine(coroutine);
+				yield return base.GameController.StartCoroutine(drawCardE);
+			}
+			else
+			{
+				base.GameController.ExhaustCoroutine(coroutine);
+				base.GameController.ExhaustCoroutine(drawCardE);
+			}
+		}
+
+
+	}
 }
