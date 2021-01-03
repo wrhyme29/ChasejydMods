6eaaa8e01e295a8b0d63d0d4b680b89fb279b448
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-02 20:28:36 -0600

    Added Intermission Controller

--- Controller/Heroes/Rockstar/Cards/IntermissionCardController.cs
+++ Controller/Heroes/Rockstar/Cards/IntermissionCardController.cs
@@ -14,6 +14,33 @@ namespace Chasejyd.Rockstar
 
         }
 
+        public override IEnumerator Play()
+        {
+            //{Rockstar} gains 2 HP. 
+            IEnumerator coroutine = GameController.GainHP(CharacterCard, 2, cardSource: GetCardSource());
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
 
+            //Search your deck or your trash for a Stage Presence card and put it into play or into your hand. If you searched your deck, shuffle your deck.
+            coroutine = SearchForCards(DecisionMaker, searchDeck: true, searchTrash: true, 1, 1, new LinqCardCriteria((Card c) => IsStagePresence(c), "stage presence"), putIntoPlay: true, putInHand: true, putOnDeck: false);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            yield break;
+        }
+
+       
     }
 }
