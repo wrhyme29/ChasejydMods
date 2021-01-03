4e7134f6ffe3ecb535f4b36c684683b8fe0430b3
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-02 20:57:55 -0600

    Added PowerChord Controller

--- Controller/Heroes/Rockstar/Cards/PowerChordCardController.cs
+++ Controller/Heroes/Rockstar/Cards/PowerChordCardController.cs
@@ -2,6 +2,7 @@
 using Handelabra.Sentinels.Engine.Model;
 using System;
 using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Chasejyd.Rockstar
@@ -14,6 +15,40 @@ namespace Chasejyd.Rockstar
 
         }
 
+        public override IEnumerator Play()
+        {
+            //Deal 1 Target Melee or Projectile Damage equal to the number of unique Ongoings {Rockstar} has in play, plus 2.
+            List<SelectDamageTypeDecision> storedResults = new List<SelectDamageTypeDecision>();
+            IEnumerator coroutine = base.GameController.SelectDamageType(base.HeroTurnTakerController, storedResults, new DamageType[]
+            {
+            DamageType.Melee,
+            DamageType.Projectile
+            }, null, SelectionType.DamageType, GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            DamageType value = storedResults.First((SelectDamageTypeDecision d) => d.Completed).SelectedDamageType.Value;
 
+            coroutine = GameController.SelectTargetsAndDealDamage(HeroTurnTakerController, new DamageSource(GameController, CharacterCard), GetNumberOfUniqueOngoingInPlay() + 2, value, 1, false, 1, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+
+        private int GetNumberOfUniqueOngoingInPlay()
+        {
+            return FindCardsWhere(c => c.IsOngoing && c.Owner == TurnTaker && c.IsInPlayAndHasGameText).Distinct().Count();
+        }
     }
 }
