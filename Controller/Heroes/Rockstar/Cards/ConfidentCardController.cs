84a81fcc31b5a2058dc7a607df2153ec917318ed
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-02 19:45:38 -0600

    Added Confident Controller

--- Controller/Heroes/Rockstar/Cards/ConfidentCardController.cs
+++ Controller/Heroes/Rockstar/Cards/ConfidentCardController.cs
@@ -11,9 +11,55 @@ namespace Chasejyd.Rockstar
 
         public ConfidentCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
-
+            SpecialStringMaker.ShowHasBeenUsedThisTurn(FirstTimeDestroy);
+            SpecialStringMaker.ShowHasBeenUsedThisTurn(FirstTimeDiscard);
         }
 
+        private const string FirstTimeDestroy = "FirstTimeDestroy";
+        private const string FirstTimeDiscard = "FirstTimeDiscard";
 
+        public override void AddTriggers()
+        {
+            //The first time each turn that one of {Rockstar}'s cards is destroyed, she may play a card.
+            AddTrigger<DestroyCardAction>((DestroyCardAction dca) => dca.WasCardDestroyed && dca.CardToDestroy != null && dca.CardToDestroy.Card.Owner == TurnTaker && !HasBeenSetToTrueThisTurn(FirstTimeDestroy), DestroyCardResponse, TriggerType.PlayCard, TriggerTiming.After);
+
+            //The first time each turn that {Rockstar} is forced to discard, she may draw a card.
+            AddTrigger<DiscardCardAction>((DiscardCardAction dca) => dca.WasCardDiscarded && dca.Origin.IsHand && dca.Origin.OwnerTurnTaker == base.TurnTaker && !HasBeenSetToTrueThisTurn(FirstTimeDiscard), DiscardCardResponse, TriggerType.DrawCard, TriggerTiming.After);
+
+            AddAfterLeavesPlayAction((GameAction ga) => ResetFlagAfterLeavesPlay(FirstTimeDiscard), TriggerType.Hidden);
+            AddAfterLeavesPlayAction((GameAction ga) => ResetFlagAfterLeavesPlay(FirstTimeDestroy), TriggerType.Hidden);
+        }
+
+        private IEnumerator DiscardCardResponse(DiscardCardAction arg)
+        {
+            SetCardPropertyToTrueIfRealAction(FirstTimeDiscard);
+            IEnumerator coroutine = DrawCard(hero: HeroTurnTaker, optional: true);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            yield break;
+        }
+
+        private IEnumerator DestroyCardResponse(DestroyCardAction dca)
+        {
+            SetCardPropertyToTrueIfRealAction(FirstTimeDestroy);
+            IEnumerator coroutine = SelectAndPlayCardFromHand(HeroTurnTakerController);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            yield break;
+        }
     }
 }
