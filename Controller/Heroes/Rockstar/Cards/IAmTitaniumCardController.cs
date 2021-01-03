cd2ca20b5ebf57a8df2494a8b1956177b16e5080
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-02 20:20:29 -0600

    Add IAmTitaniumController

--- Controller/Heroes/Rockstar/Cards/IAmTitaniumCardController.cs
+++ Controller/Heroes/Rockstar/Cards/IAmTitaniumCardController.cs
@@ -14,6 +14,27 @@ namespace Chasejyd.Rockstar
 
         }
 
+        public override void AddTriggers()
+        {
+            //Reduce damage dealt to {Rockstar} by 1.
+            AddReduceDamageTrigger((Card c) => c == CharacterCard, 1);
 
+            //When {Rockstar} is dealt Melee or Projectile Damage, she may deal 1 damage of that type to one Non-Hero Target.
+            AddTrigger<DealDamageAction>((DealDamageAction dd) => dd.Target == CharacterCard && dd.DidDealDamage && (dd.DamageType == DamageType.Melee || dd.DamageType == DamageType.Projectile), DealCounterDamageResponse, TriggerType.DealDamage, TriggerTiming.After);
+        }
+
+        private IEnumerator DealCounterDamageResponse(DealDamageAction dd)
+        {
+            IEnumerator coroutine = GameController.SelectTargetsAndDealDamage(HeroTurnTakerController, new DamageSource(GameController, CharacterCard), 1, dd.DamageType, 1, false, 0, additionalCriteria: (Card c) => !c.IsHero && c.IsTarget, cardSource: GetCardSource());
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
     }
 }
