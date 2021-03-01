f8e7ad89e2701f752022d47651915a0969411e35
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-02-28 22:21:19 -0600

    Added Blindside Controller + Tests

--- Controller/Heroes/Headlong/Cards/BlindsideCardController.cs
+++ Controller/Heroes/Headlong/Cards/BlindsideCardController.cs
@@ -1,6 +1,6 @@
 ﻿using Handelabra.Sentinels.Engine.Controller;
 using Handelabra.Sentinels.Engine.Model;
-using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -12,6 +12,38 @@ namespace Chasejyd.Headlong
         public BlindsideCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
 
+        }
+
+        public override IEnumerator Play()
+        {
+            int X = GetNumberOfEnvironmentCardsInPlay() + 1;
+            //Deal one Non-Hero Target X Melee Damage where X is equal to the number of Environment Cards in play +1. 
+            IEnumerator coroutine = GameController.SelectTargetsAndDealDamage(HeroTurnTakerController, new DamageSource(GameController, CharacterCard), X, DamageType.Melee, 1, false, 1, additionalCriteria: (Card c) => !c.IsHero, cardSource: GetCardSource());
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+
+            //Each other Player may Draw a Card.
+            coroutine = EachPlayerDrawsACard(heroCriteria: htt => htt != HeroTurnTaker);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+
+        private int GetNumberOfEnvironmentCardsInPlay()
+        {
+            return FindCardsWhere(c => c.IsEnvironment && c.IsInPlayAndHasGameText && c.IsRealCard).Count();
         }
 
 
