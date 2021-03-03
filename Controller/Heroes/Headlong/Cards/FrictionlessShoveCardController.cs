381d8d2554dc8f90a677852a9156888ef57cfa25
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-03-03 01:19:03 -0600

    Added FrictionlessShove Controller

--- Controller/Heroes/Headlong/Cards/FrictionlessShoveCardController.cs
+++ Controller/Heroes/Headlong/Cards/FrictionlessShoveCardController.cs
@@ -14,6 +14,46 @@ namespace Chasejyd.Headlong
 
         }
 
+        public override IEnumerator Play()
+        {
+            //Place a non-Character Villain Target in play on the top of its Deck. 
+            List<SelectCardDecision> storedResults = new List<SelectCardDecision>();
+            IEnumerator coroutine = base.GameController.SelectCardAndStoreResults(HeroTurnTakerController, SelectionType.MoveCardOnDeck, new LinqCardCriteria((Card c) => IsVillain(c) && c.IsInPlay && !c.IsCharacter &&  !c.IsOneShot && base.GameController.IsCardVisibleToCardSource(c, GetCardSource()), "non-character villain cards in play", useCardsSuffix: false), storedResults, optional: false, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+           if(DidSelectCard(storedResults))
+            {
+                Card selectedCard = GetSelectedCard(storedResults);
+                coroutine = GameController.MoveCard(TurnTakerController, selectedCard, selectedCard.NativeDeck, showMessage: true, cardSource: GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+
+            //{Headlong} may Deal a non-Character Target 3 Projectile Damage.
+            coroutine = GameController.SelectTargetsAndDealDamage(HeroTurnTakerController, new DamageSource(GameController, CharacterCard), 3, DamageType.Projectile, 1, false, 0, additionalCriteria: (Card c) => !c.IsCharacter, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
 
     }
 }
