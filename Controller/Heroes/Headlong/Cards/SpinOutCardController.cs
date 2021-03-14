02d1bfde763e78b5401c9c7b7235e7abca641bf7
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-03-13 19:58:46 -0600

    Added SpinOut Controller

--- Controller/Heroes/Headlong/Cards/SpinOutCardController.cs
+++ Controller/Heroes/Headlong/Cards/SpinOutCardController.cs
@@ -1,6 +1,6 @@
 ﻿using Handelabra.Sentinels.Engine.Controller;
 using Handelabra.Sentinels.Engine.Model;
-using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -8,10 +8,39 @@ namespace Chasejyd.Headlong
 {
     public class SpinOutCardController : HeadlongCardController
     {
-
         public SpinOutCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
 
+        }
+
+        public override IEnumerator Play()
+        {
+            //{Headlong} deals 1 Non-Hero Target 4 Melee Damage. Then he may deal a second Non-Hero Target 3 Projectile Damage.
+
+            List<DealDamageAction> storedResults = new List<DealDamageAction>();
+            IEnumerator coroutine = GameController.SelectTargetsAndDealDamage(HeroTurnTakerController, new DamageSource(GameController, CharacterCard), 4, DamageType.Melee, 1, false, 1, additionalCriteria: (Card c) => !c.IsHero && c.IsTarget, storedResultsDamage: storedResults, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            if(DidDealDamage(storedResults))
+            {
+                Card damagedTarget = storedResults.First().Target;
+                coroutine = GameController.SelectTargetsAndDealDamage(HeroTurnTakerController, new DamageSource(GameController, CharacterCard), 3, DamageType.Projectile, 1, false, 0, additionalCriteria: (Card c) => c != damagedTarget && !c.IsHero && c.IsTarget, storedResultsDamage: storedResults, cardSource: GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+            yield break;
         }
 
 
