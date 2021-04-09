af9b8ef006a39f3979aa75231dea1a0293c9e4d4
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-04-09 04:27:11 -0500

    Added SteadyRhythm Controller +Tests

--- Controller/TeamVillains/DeeprootTeam/Cards/StranglevinesCardController.cs
+++ Controller/TeamVillains/DeeprootTeam/Cards/StranglevinesCardController.cs
@@ -3,6 +3,7 @@ using Handelabra.Sentinels.Engine.Model;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Chasejyd.DeeprootTeam
 {
@@ -14,6 +15,37 @@ namespace Chasejyd.DeeprootTeam
 
         }
 
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+        }
+
+        public override IEnumerator DeterminePlayLocation(List<MoveCardDestination> storedResults, bool isPutIntoPlay, List<IDecision> decisionSources, Location overridePlayArea = null, LinqTurnTakerCriteria additionalTurnTakerCriteria = null)
+        {
+            if (storedResults is null)
+            {
+                yield break;
+            }
+
+            List<Card> foundTarget = new List<Card>();
+            IEnumerator coroutine = base.GameController.FindTargetsWithHighestHitPoints(1, 1, (Card c) => c.IsHeroCharacterCard && GameController.IsCardVisibleToCardSource(c, GetCardSource()) && (overridePlayArea == null || c.IsAtLocationRecursive(overridePlayArea)), storeHighest: foundTarget, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            Card highest = foundTarget.FirstOrDefault();
+            if (highest != null)
+            {
+                //Play this card next to the Hero Character with the highest HP. 
+                storedResults.Add(new MoveCardDestination(highest.NextToLocation));
+            }
+
+            yield break;
+        }
 
     }
 }
